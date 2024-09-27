6325c0a5df576eee60c108a717e8a2c61a4838b6
O-orion <lucas.lvtorres@gmail.com>
2024-09-27 12:37:34 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+  import HomePage from './views/Home.vue';
+</script>
+
+<template>
+  <HomePage></HomePage>
+</template>
+
---
+++ src/components/NavBar.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+</template>
+
+<style src="../styles/components/navbar.css"></style>
