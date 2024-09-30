be9de3ca715f41f839ea57cda7f340e59802a540
O-orion <lucas.lvtorres@gmail.com>
2024-09-30 14:43:11 -0300

    feat: Criando testes unitários para o componente PokemonAbilities
    -- Verificando se componente foi renderizado
    -- Verificando se background passado pela props foi aplicado aos elementos
    -- Verificando se o nome das abilidades foram exibidas corretamente

---
+++ src/tests/PokemonAbilities.spec.ts
@@ -0,0 +1,49 @@
+import { mount } from "@vue/test-utils";
+import PokemonAbilities from "../components/PokemonAbilities.vue";
+
+describe('PokemonAbilities.vue', () => {
+
+    it ('Verificando se o componente foi renderizado', () => {
+        const wrapper = mount(PokemonAbilities, {
+            props: {
+                abilities: [ { ability: { name: 'voar'} }],
+                background: '#cccc'
+            }
+        })
+
+        const containerAbilities = wrapper.find('.container-abilities');
+
+        expect(containerAbilities.exists()).toBe(true)
+
+    })
+
+    it('Verificando se a abilidade foi renderizada', () => {
+        const wrapper = mount(PokemonAbilities, {
+            props: {
+                abilities: [ { ability: { name: 'voar'} },{ ability: { name: 'nadar'} }],
+                background: '#cccc'
+            }
+        })
+
+        const textAbilidade = wrapper.findAll('.pokemon-abilitie p')
+
+        expect(textAbilidade[0].text()).toBe('Voar')
+        expect(textAbilidade[1].text()).toBe('Nadar')
+    })
+
+    it ('Deve aplicar o backgroundColor corretamente com a prop', () => {
+        const wrapper = mount(PokemonAbilities, {
+            props: {
+                abilities: [ { ability: { name: 'voar'} },{ ability: { name: 'nadar'} }],
+                background: '#cccc'
+            }
+        })
+
+        const elements = wrapper.findAll('.pokemon-abilitie');
+        elements.forEach((element) => {
+            expect(element.attributes('style')).toContain('background-color: rgba(204, 204, 204, 0.8)')
+        })
+    })
+
+})
+
