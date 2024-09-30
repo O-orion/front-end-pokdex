33eff77e7d2b283263f0b468827862c8b61263db
O-orion <lucas.lvtorres@gmail.com>
2024-09-30 17:31:54 -0300

    refactoring: Dividindo as responsabilidades do componente PokemonDetail
    -- Foi Criado os componentes PokemonDetailTitulo

--- src/components/PokemonDetail.vue
+++ src/components/PokemonDetail.vue
@@ -1,21 +1,17 @@
 <script setup lang="ts">
 import { defineProps } from 'vue';
-
-import { TypesPokemon } from '../types/Types.enum';
 import PokemonAbilities from './PokemonAbilities.vue';
 import PokemonStatus from './PokemonStatus.vue';
 import PokemonMoves from './PokemonMoves.vue';
 import PokemonShiny from './PokemonShiny.vue';
 import { PokemonDetailsType } from '../types/PokemonDetails.type';
-import PokemonTypes from './PokemonTypes.vue';
-import { capitalizeFirstLetter } from '../utils/methods';
 import { getTypeColor } from '../utils/methods';
 import PokemonDetailHeader from './PokemonDetailHeader.vue';
+import PokemonDetailTitulo from './PokemonDetailTito.vue';
 
 const props = defineProps<{
     pokemon: PokemonDetailsType | null
 }>()
-
 
 </script>
 
@@ -24,20 +20,20 @@ const props = defineProps<{
         <PokemonDetailHeader :pokemon="pokemon" ></PokemonDetailHeader>
 
         <section class="pokemon-body">
-            <h2 class="pokemon-habilities">Habilidades</h2>
+            <PokemonDetailTitulo titulo="Habilidades" ></PokemonDetailTitulo>
             <div :style="{ backgroundColor: getTypeColor(pokemon?.types[0].type.name) }" class="borda"></div>
             <PokemonAbilities :background="getTypeColor(pokemon?.types[0].type.name)" :abilities="pokemon?.abilities">
             </PokemonAbilities>
 
-            <h2 class="pokemon-habilities">Estatisticas</h2>
+            <PokemonDetailTitulo titulo="Estatísticas" ></PokemonDetailTitulo>
             <div :style="{ backgroundColor: getTypeColor(pokemon?.types[0].type.name) }" class="borda"></div>
             <PokemonStatus :status="pokemon?.stats"></PokemonStatus>
 
-            <h2 class="pokemon-habilities">Moves</h2>
+            <PokemonDetailTitulo titulo="Moves" ></PokemonDetailTitulo>
             <div :style="{ backgroundColor: getTypeColor(pokemon?.types[0].type.name) }" class="borda"></div>
             <PokemonMoves :background="getTypeColor(pokemon?.types[0].type.name)" :moves="pokemon?.moves"></PokemonMoves>
 
-            <h2 class="pokemon-habilities">Shiny</h2>
+            <PokemonDetailTitulo titulo="Shinys" ></PokemonDetailTitulo>
             <div :style="{ backgroundColor: getTypeColor(pokemon?.types[0].type.name) }" class="borda"></div>
             <PokemonShiny :shiny="pokemon?.sprites"></PokemonShiny>
 
---
+++ src/components/PokemonDetailTito.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts" >
+    import { defineProps } from 'vue';
+
+    const props = defineProps<{
+        titulo: string
+    }>()
+
+</script>
+
+<template>
+    <h2 class="pokemon-habilities"> {{ titulo }} </h2>
+</template>
+
+
+<style scoped src="../styles/components/pokemonDetailsTitulo.css" ></style>
