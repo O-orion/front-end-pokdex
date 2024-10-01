e117fced1a104c4df0a3b2071e7249a722bb67e9
O-orion <lucas.lvtorres@gmail.com>
2024-09-30 21:58:21 -0300

    feat: Criando testes unitários para componente TituloPokedex

--- src/tests/PokemonDetail.spec.ts
+++ src/tests/PokemonDetail.spec.ts
@@ -124,6 +124,4 @@ describe('PokemonDetail.vuew', () => {
 
     })
 
-    
-
 })
---
+++ src/tests/TitutloPokedex.spec.ts
@@ -0,0 +1,25 @@
+import {  mount } from '@vue/test-utils';
+import TituloPokedex from '../components/TituloPokedex.vue';
+
+describe('TituloPokedex.vue', () => {
+    
+    it('Verificando se o componente foi renderizado', () => {
+        const wrapper = mount(TituloPokedex);
+
+        const element = wrapper.find('.container');
+
+        expect(element.exists()).toBe(true);
+
+    })
+
+    it('Verificando se o titulo está sendo exibido', () => {
+        const wrapper = mount(TituloPokedex);
+
+        const textComponent = wrapper.find('.titulo');
+
+        expect(textComponent.exists()).toBe(true);
+
+        expect(textComponent.text()).toBe('Pokédex')
+
+    })
+})
